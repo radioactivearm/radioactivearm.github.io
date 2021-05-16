425e659e006a21506b35ec6e4c5627f4f2d8726b
arm <andy.mcrae21@gmail.com>
2021-05-16 17:05:08 -0500

    made some css changes and made link images height responsive

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log('loaded main.js');
+
+
+// =================================================
+// Adjust height of Images above links to match width
+// made reactive to resize of browser.
+function scaleImages() {
+
+    var linkImage = d3.selectAll('.link-image');
+    var linkImageWidth = linkImage.property('width');
+
+    console.log(linkImageWidth);
+
+    linkImageHeight = 2.5 * linkImageWidth / 4;
+
+    linkImage.property('height', linkImageHeight);
+}
+
+scaleImages()
+
+d3.select(window).on("resize", scaleImages);
+// =================================================
